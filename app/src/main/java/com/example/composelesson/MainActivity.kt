565fb93d9fd28306298b3617db97de2b24180c3e
Nurislam <gggrrraaa1235@gmail.com>
2023-09-23 14:59:39 +0600

    init

---
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.composelesson
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelesson.ui.theme.ComposeLessonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLessonTheme {
+                helloWorld()
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun helloWorld() {
+    Text(text = "Hello World")
+}
+
+
+
+
+
