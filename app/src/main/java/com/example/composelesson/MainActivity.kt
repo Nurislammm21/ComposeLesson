e2bbbc8b73adda2b6bbe77e89f6ed43613cfb9c7
Nurislam <gggrrraaa1235@gmail.com>
2023-09-25 21:17:25 +0600

    practice with row elements and move it

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composelesson.ui.theme.ComposeLessonTheme
 
@@ -21,7 +27,7 @@ class MainActivity : ComponentActivity() {
                 Surface( modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        GreetingText(message = "Happy birthday Nurislam!")
+                        GreetingText(message = "Happy birthday Nurislam!", from = "From Brother")
                 }
 
             }
@@ -33,17 +39,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardView(){
     ComposeLessonTheme{
-            GreetingText(message = "Happy Birthday Nurislam!")
+            GreetingText(message = "Happy Birthday Nurislam!", from = "From Brother")
     }
 }
 
 @Composable
-fun GreetingText(message : String, modifier : Modifier = Modifier){
+fun GreetingText(message : String, from : String, modifier : Modifier = Modifier){
+    Column(verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)){
     Text(text = message,
-        fontSize = 90.sp,
-        lineHeight = 116.sp
+        fontSize = 79.sp,
+        lineHeight = 94.sp,
+        textAlign = TextAlign.Center
         )
+    Text(text = from,
+        fontSize = 28.sp,
+        modifier = modifier.padding(16.dp).align(alignment = Alignment.End)
+    )
 }
-
+}
 
 
