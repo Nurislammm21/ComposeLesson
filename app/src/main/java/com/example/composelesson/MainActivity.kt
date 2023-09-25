7394d170ecca03a8966a430eb5f6a85a12433330
Nurislam <gggrrraaa1235@gmail.com>
2023-09-25 20:25:13 +0600

    first connecting special Annotations for the project part 2

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelesson.ui.theme.ComposeLessonTheme
 
@@ -13,7 +17,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLessonTheme {
-
+                Surface( modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                            Greeting("Android")
+                }
 
             }
         }
@@ -24,13 +32,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardView(){
     ComposeLessonTheme{
-            Greeting(name = "Android")
+            Greeting(name = "Nurislam")
     }
 }
 
 @Composable
-fun Greeting(name : String){
-    Text("Hello $name!")
+fun Greeting(name : String, modifier : Modifier = Modifier){
+    Text("Hello $name!",
+        modifier = modifier
+        )
 }
 
 
