2d2021dec6cd30a4e696c8057c8997cd65c8b738
Nurislam <gggrrraaa1235@gmail.com>
2023-09-25 15:30:31 +0600

    from now starts to begin pathway 3

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelesson.ui.theme.ComposeLessonTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,26 +10,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLessonTheme {
-                helloWorld()
+
 
             }
         }
     }
 }
-//Nurislam  1 2 3 4 5 56 67 7
-//ghjg
-
-@Composable
-fun newFun() {
-    //NNN
-}
 
 
-@Preview
-@Composable
-fun helloWorld() {
-    Text(text = "Hello World")
-}
 
 
 
