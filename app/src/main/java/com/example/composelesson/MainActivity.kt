2458735da91cd3db3011a3d65fee5c70165e7268
Nurislam <gggrrraaa1235@gmail.com>
2023-09-26 20:26:30 +0600

    Adding an Image

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,13 +37,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardView(){
-    ComposeLessonTheme{
-            GreetingText(message = "Happy Birthday Nurislam!", from = "From Brother")
-    }
-}
+
 
 @Composable
 fun GreetingText(message : String, from : String, modifier : Modifier = Modifier){
@@ -54,9 +50,25 @@ fun GreetingText(message : String, from : String, modifier : Modifier = Modifier
         )
     Text(text = from,
         fontSize = 28.sp,
-        modifier = modifier.padding(16.dp).align(alignment = Alignment.End)
+        modifier = modifier
+            .padding(16.dp)
+            .align(alignment = Alignment.End)
     )
 }
 }
 
+@Composable
+fun GreetingImage(message : String, from : String, modifier : Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Image(painter =image ,
+        contentDescription =null )
+}
 
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardView(){
+    ComposeLessonTheme{
+        GreetingImage(message = "Happy Birthday Nurislam!", from = "From Brother")
+    }
+}
