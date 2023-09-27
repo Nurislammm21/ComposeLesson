70f917ab0b0c2cfe5e915c98d008ee355a7f4170
Nurislam <gggrrraaa1235@gmail.com>
2023-09-27 22:39:35 +0600

    Started making task manager

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,10 +32,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Column() {
+                Surface( modifier = Modifier.fillMaxSize()) {
 
-                    addImage()
-                    addingText()
+                    AcceptImage()
 
 
                 }
@@ -42,36 +43,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
-fun addImage(){
-    Box( modifier = Modifier
-    ){
-       Image(painter = painterResource(id = R.drawable.bg_compose_background), contentDescription = "Image at the top",
-            
-           )
+fun AcceptImage(){
+    Column( modifier = Modifier.fillMaxHeight().fillMaxWidth(),verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ){
+               Image(painter = painterResource(id = R.drawable.ic_task_completed), contentDescription = "Image ")
+        Text(text = "All tasks completed",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp))
 
     }
+
+
+}
+
+@Composable
+ fun GreetingText(){
+
 }
 
 
-@Composable
-fun addingText(){
-    Column {
-        Text(text = "Jetpack Compose tutorial",
-                fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black,
-                    modifier = Modifier.padding(16.dp)
-
-            )
-            Text(text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                fontSize = 18.sp,
-                modifier = Modifier.padding(16.dp)
-                )
-                Text(text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(16.dp)
-                    )
-    }
-}
