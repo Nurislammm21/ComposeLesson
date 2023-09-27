80303bb2681e2d9b3ce07fd85feb163e6025bbae
Nurislam <gggrrraaa1235@gmail.com>
2023-09-27 21:23:55 +0600

    Compose article part 1 finishing image and first text

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,60 +30,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLessonTheme {
-                Surface( modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        GreetingImage(message = getString(R.string.happy_birthday_text), from = getString(
-                                                    R.string.from_someone))
+                Column() {
+
+                    addImage()
+                    addingText()
+
+
                 }
 
-            }
         }
     }
 }
 
 
-
 @Composable
-fun GreetingText(message : String, from : String, modifier : Modifier = Modifier){
-    Column(verticalArrangement = Arrangement.Center,
-        modifier = modifier){
-    Text(text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.padding(top = 16.dp)
-        )
-    Text(text = from,
-        fontSize = 36.sp,
-        modifier = Modifier
-            .padding(top = 16.dp)
-            .padding(end = 16.dp)
-            .align(alignment = Alignment.End)
-    )
-}
-}
-
-@Composable
-fun GreetingImage(message : String, from : String, modifier : Modifier = Modifier){
-    val image = painterResource(R.drawable.androidparty)
-    Box{
-        Image(painter =image ,
-            contentDescription =null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-            )
-        GreetingText(message = message, from = from, modifier = Modifier.fillMaxSize().padding(8.dp))
+fun addImage(){
+    Box( modifier = Modifier
+    ){
+       Image(painter = painterResource(id = R.drawable.bg_compose_background), contentDescription = "Image at the top",
+            
+           )
 
     }
 }
 
 
-@Preview(showBackground = true)
 @Composable
-fun BirthdayCardView(){
-    ComposeLessonTheme{
-        GreetingImage(message = "Happy Birthday Nurislam!", from = "From Brother")
+fun addingText(){
+    Column() {
+        Text(text = "Jetpack Compose tutorial",
+                fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(16.dp)
+
+            )
     }
 }
