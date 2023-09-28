cc891160e09ec7946ea11a82637199a2ae2f35dd
Nurislam <gggrrraaa1235@gmail.com>
2023-09-28 20:19:32 +0600

    Creating compose quadrant finished

--- app/src/main/java/com/example/composelesson/MainActivity.kt
+++ app/src/main/java/com/example/composelesson/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.composelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,20 +17,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composelesson.ui.theme.ComposeLessonTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(modifier = Modifier.fillMaxSize()) {
+            Surface(     modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background) {
 
                 ComposeQuadrant()
 
@@ -46,8 +42,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ComposeQuadrant() {
         Column(modifier = Modifier.fillMaxWidth()){
-            Row(Modifier.weight(1F)){
-
+            Row(Modifier.weight(1f)){
+                OneColumn(title = stringResource(R.string.first_title), 
+                    description = stringResource(id = R.string.first_description) ,
+                    backgroundColor = Color(0xFFEADDFF),
+                    modifier = Modifier.weight(1f)
+                    )
+                    OneColumn(title = stringResource(R.string.second_title),
+                        description = stringResource(R.string.second_description),
+                        backgroundColor = Color(0xFFD0BCFF),
+                        modifier = Modifier.weight(1f))
+            }
+            Row(Modifier.weight(1f)){
+                OneColumn(title = stringResource(R.string.third_title),
+                    description = stringResource(R.string.third_description),
+                    backgroundColor = Color(0xFFB69DF8),
+                    modifier = Modifier.weight(1f))
+                
+                OneColumn(title = stringResource(R.string.fourth_title),
+                    description = stringResource(R.string.fourth_description),
+                    backgroundColor = Color(0xFFF6EDFF),
+                    modifier = Modifier.weight(1f))
             }
         }
 
@@ -62,7 +77,7 @@ fun OneColumn(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(backgroundColor)
             .padding(16.dp),
